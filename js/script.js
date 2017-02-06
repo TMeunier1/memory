36f4ef26a7002b14a1c648674c57e38741552d9d
karina BRANTE <karina.brante@gmail.com>
2017-02-06 15:53:21 +0100

    numeros

--- js/script.js
+++ js/script.js
@@ -3,9 +3,27 @@ var board = document.getElementById("board");
 
 
 
-for (var i = 0; i < nombreDeCarte; i++) {
+for (var i = 1; i <= nombreDeCarte/2; i++) {
+
     var carte = document.createElement("div");
-    carte.className="card"
+    carte.className="card";
     carte.innerHTML = i;
     board.appendChild(carte);
+
+    var carte = document.createElement("div");
+    carte.className="card";
+    carte.innerHTML = i;
+    board.appendChild(carte);
+
+};
+for (var i = 1; i <= nombreDeCarte/2; i++) {
+
+    var carte = document.createElement("div");
+    carte.className="cache";
+    board.appendChild(carte);
+
+    var carte = document.createElement("div");
+    carte.className="cache";
+    board.appendChild(carte);
+
 }
