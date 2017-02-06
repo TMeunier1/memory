f97e06a0f976cb8a8028b1c63b9ce846779ec329
Tibau <xiliar1888@hotmail.fr>
2017-02-06 16:44:41 +0100

    ajout d'event click

--- js/script.js
+++ js/script.js
@@ -1,29 +1,37 @@
 var nombreDeCarte = "12";
 var board = document.getElementById("board");
-
+var carte
 
 
 for (var i = 1; i <= nombreDeCarte/2; i++) {
 
-    var carte = document.createElement("div");
-    carte.className="card";
+    carte = document.createElement("div");
+    carte.className="cache";
     carte.innerHTML = i;
     board.appendChild(carte);
 
-    var carte = document.createElement("div");
-    carte.className="card";
+    carte.addEventListener("click", function() {
+            if (this.className == "cache") {
+                this.className = "card";
+            }
+
+            else {
+                this.className = "cache";
+            }
+    });
+
+    carte = document.createElement("div");
+    carte.className="cache";
     carte.innerHTML = i;
     board.appendChild(carte);
 
+    carte.addEventListener("click", function() {
+            if (this.className == "cache") {
+                this.className = "card";
+            }
+
+            else {
+                this.className = "cache";
+            }
+    });
 };
-for (var i = 1; i <= nombreDeCarte/2; i++) {
-
-    var carte = document.createElement("div");
-    carte.className="cache";
-    board.appendChild(carte);
-
-    var carte = document.createElement("div");
-    carte.className="cache";
-    board.appendChild(carte);
-
-}
