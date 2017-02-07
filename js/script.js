662bc63ecbd50214d68a44e211508d3ec27556e1
karina BRANTE <karina.brante@gmail.com>
2017-02-07 10:09:30 +0100

    deuxieme div

--- js/script.js
+++ js/script.js
@@ -2,13 +2,18 @@ var nombreDeCarte = "12";
 var board = document.getElementById("board");
 var carte;
 var carte2 = null;
+var div2 = "";
+var divtexte = "";
 
 
 for (var i = 1; i <= nombreDeCarte/2; i++) {
 
     carte = document.createElement("div");
     carte.className="cache";
-    carte.innerHTML = i;
+    divtexte = document.createElement("div");
+    board.appendChild(divtexte);
+    divtexte.className = "card";
+    divtexte = i;
     board.appendChild(carte);
 
     carte.addEventListener("click", function() {
@@ -32,6 +37,10 @@ for (var i = 1; i <= nombreDeCarte/2; i++) {
     carte.className="cache";
     carte.innerHTML = i;
     board.appendChild(carte);
+    divtexte = document.createElement("div");
+    board.appendChild(divtexte);
+    divtexte.className = "card";
+    divtexte = i;
 
     carte.addEventListener("click", function() {
             if (this.className == "cache") {
